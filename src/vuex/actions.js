f8e672dae79bca444f70b7f4780c0799dbb44fe1
XiNGRZ <chenxingyu92@gmail.com>
2020-01-30 15:03:22 +0800

    Clean up for v4

--- src/vuex/actions.js
+++
@@ -1,7 +0,0 @@
-export const sync = ({ dispatch }, from, source) => {
-  dispatch('SYNC', { from, source })
-}
-
-export const select = ({ dispatch }, from, start, end) => {
-  dispatch('SELECT' , { from, start, end })
-}
